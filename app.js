680f769875ceda137eda8e6621e47abc68ee228b
harshit3598 <68678320+harshit3598@users.noreply.github.com>
2021-10-18 23:43:06 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const passport = require('passport');
+require('dotenv').config();
+
+var app = express();
+
+require('./config/database');
+require('./models/user');
+require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/users',require('./routes/users'));
+app.listen(3000, ()=> console.log("Server running on 3000!"));
